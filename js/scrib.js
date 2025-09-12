8f8192a777fc5972d5fa42604e7bb44aea7af4e5
Oscar Vargas <vargasoscarjulian595@gmail.com>
2025-09-12 15:08:55 -0500

    Actualizando

--- js/scrib.js
+++
@@ -1,7 +0,0 @@
-const botones = document.querySelectorAll('.btncomprar');
-
-botones.forEach(boton => {
-    boton.addEventListener('click', () => {
-        alert('Producto agregado al carrito');
-    });
-});
